933ddc9ab48c9bebe357b1e61d24f33a5b80688b
daixiaowei <daixiaowei@zhixuezhen.com>
2021-02-20 15:45:10 +0800

    Init repo

---
+++ core/src/me/dxw/wow/WoW.java
@@ -0,0 +1,32 @@
+package me.dxw.wow;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class WoW extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(Color.RED);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/me/dxw/wow/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package me.dxw.wow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.dxw.wow.WoW;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 640;
+		new LwjglApplication(new WoW(), config);
+	}
+}
